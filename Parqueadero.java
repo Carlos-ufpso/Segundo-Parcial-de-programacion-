d4539fbc4916f5a55c0471ad6a7783566eab4834
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-22 07:33:34 -0500

    (feat): Se creo un constructor para inicializar el objeto EspacioParqueadero y se crearon metodos para devolver los atributos de la clase EspacioParqueadero

--- Parqueadero.java
+++ Parqueadero.java
@@ -25,9 +25,51 @@ public String toString() {
 }
 }
 
-
 class EspacioParqueadero {
     private int numeroEspacio;
     private String tipoPermitido;
     private boolean estado;
+
+public EspacioParqueadero(int numeroEspacio, String tipoPermitido) {
+    this.numeroEspacio = numeroEspacio;
+    this.tipoPermitido = tipoPermitido;
+    this.estado = false;
+}
+
+public int getNumeroEspacio() {
+    return numeroEspacio;
+}
+
+public String getTipoPermitido() {
+    return tipoPermitido;
+}
+
+public boolean getEstado() {
+    return estado;
+}
+
+public void asignarVehiculo(Vehiculo vehiculo) {
+    if (estado) {
+        System.out.println("El espacio ya está ocupado");
+    } else if (!vehiculo.getTipo().equals(tipoPermitido)) {
+        System.out.println("El vehículo no puede estacionarse aquí. Tipo de espacio incorrecto");
+    } else {
+        estado = true;
+        System.out.println("Vehículo asignado al espacio " + numeroEspacio + ": " + vehiculo);
+    }
+}
+
+public void liberarEspacio() {
+    if (!estado) {
+        System.out.println("El espacio ya está disponible");
+    } else {
+        estado = false;
+        System.out.println("El espacio " + numeroEspacio + " ha sido liberado");
+    }
+}
+
+@Override
+public String toString() {
+    return "Espacio " + numeroEspacio + " (" + tipoPermitido + "): " + (estado ? "Ocupado" : "Disponible");
+}
 }
