8e9388ad838d033f33e06f9d92958bbb2c9622e0
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-22 07:13:08 -0500

    (feat): Se creo un constructor para inicializar el objeto y se crearon los metodos para devolver los atributos de la clase

--- Parqueadero.java
+++ Parqueadero.java
@@ -3,4 +3,24 @@ class Vehiculo {
     private String placa;
     private String tipo;
     private String propietario;
+
+
+public Vehiculo(String placa, String tipo, String propietario) {
+    this.placa = placa;
+    this.tipo = tipo;
+    this.propietario = propietario;
+}
+public String getPlaca() {
+    return placa;
+}
+public String getTipo() {
+    return tipo;
+}
+public String getPropietario() {
+    return propietario;
+}
+@Override
+public String toString() {
+    return "Placa: " + placa + ", Tipo: " + tipo + ", Propietario: " + propietario;
+}
 }
